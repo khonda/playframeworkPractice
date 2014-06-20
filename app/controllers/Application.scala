d7a12d63714c892a15b2895d082ab017fdf92c24
khonda <kou.c.honda@gmail.com>
2014-06-20 15:32:10 +0900

    session

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -30,7 +30,21 @@ object Application extends Controller {
     Task.delete(id)
     Redirect(routes.Application.tasks)
   }
-  
+
+  def sample1(id: Long)=TODO
+  def sample2(id: Long) = Action {
+    Ok(views.html.sample(id))
+  }
+  def sample3(id: Long) = Action {
+    Ok(views.html.sample(id))
+  }
+  def sample4(id: Option[Int]) = Action {
+    val res = id match{
+      case Some(x) => x
+      case None => 99
+    }
+    Ok(views.html.sample(res))
+  }
   val taskForm = Form(
     "label" -> nonEmptyText
   )
---
+++ app/controllers/SessionController.scala
@@ -0,0 +1,39 @@
+package controllers
+
+import java.lang.ProcessBuilder.Redirect
+
+import play.api._
+import play.api.mvc._
+import play.api.libs.iteratee.Enumerator
+
+/**
+ * Created by 4006846 on 2014/06/20.
+ */
+object SessionController extends Controller{
+  def setSession =Action{ implicit request =>
+    val data = new java.util.Date().toString()
+    Ok("save session:" + data).withSession(
+      session + ("date" -> data)
+    )
+  }
+
+  def getSession = Action { implicit request =>
+    session.get("date").map{ date =>
+      Ok("save session page access time:" + date)
+    }.getOrElse{
+      Ok("you have never access in save session page.")
+    }
+  }
+
+  def getFlash = Action { implicit  request =>
+    Ok {
+      flash.get("msg").getOrElse("no msg!")
+    }
+  }
+
+  def setFlash = Action {
+    val data = new java.util.Date().toString()
+    Redirect("/readFlash").flashing("msg"->data)
+  }
+
+}
